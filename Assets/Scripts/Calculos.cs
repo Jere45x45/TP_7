d0b6fa83e00a102c4fe1d0903ff940ebfdc191ab
Jere45x45 <jerebeyo@gmail.com>
2026-04-17 12:03:12 -0300

    Listo

---
+++ Assets/Scripts/Calculos.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calculos : MonoBehaviour
+{
+    public int num1 = 24;
+    public int num2 = 12;
+    public int resultado;
+    // Start is called before the first frame update
+    void Start()
+    {
+        resultado = num1 + num2;
+        Debug.Log("El resultado de sumar " + num1 + " y " + num2 + " es: " + resultado);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/CalculosPersonalizados.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculosPersonalizados : MonoBehaviour
+{
+    public float num1 = 4;
+    public float num2 = 2;
+    public float resultado; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        resultado = num1 + num2;
+        Debug.Log("El resultado de sumar " + num1 + " y " + num2 + " es: " + resultado);
+        resultado = num1 - num2;
+        Debug.Log("El resultado de restar " + num1 + " y " + num2 + " es: " + resultado);
+        resultado = num1 * num2;
+        Debug.Log("El resultado de multiplicar " + num1 + " y " + num2 + " es: " + resultado);
+        resultado = num1 / num2;
+        Debug.Log("El resultado de dividir " + num1 + " y " + num2 + " es: " + resultado);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
