688c0ae4b99a56c9aeedd9d6621d3564e12e9ced
Jere45x45 <jerebeyo@gmail.com>
2026-04-17 09:07:20 -0300

    Initial commit

---
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosDelUsuario : MonoBehaviour
+{
+    public string nombre = "Pepe";
+    public int edad = 44;
+    public float estatura = 1.8f;
+    public bool esONoDonante = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Mi nombre es " + nombre + ", tengo " + edad + ", mido " + estatura + " metros y es " + esONoDonante + " que soy donante de órganos.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeclaracionDeVariables : MonoBehaviour
+{
+    public int numeroInt = 1;
+    public float numeroFloat = 2.1f;
+    public string cadenaDeTexto = "DeclaracionDeVariable";
+    public bool booleano = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     Debug.Log(numeroInt);
+     Debug.Log(numeroFloat);
+     Debug.Log(cadenaDeTexto);
+     Debug.Log(booleano);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
